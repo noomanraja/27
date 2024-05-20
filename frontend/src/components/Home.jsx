d304d60c370eb950a277df95c4401170e8ec1299
noomanraja <nooman9@hotmail.com>
2024-05-21 01:13:06 +0200

    a

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
-  const [pokemons, setPokemons] = useState([])
-  const [types, setTypes] = useState([])
+  const [pokemons, setPokemons] = useState([]);
+  const [types, setTypes] = useState([]);
 
   useEffect(() => {
-    
+
+
+  }, []); 
+};
+
+export default Home;
